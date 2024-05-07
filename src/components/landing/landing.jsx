541928699f4642c2ce593ad0c89ad440357b5e2e
bdulong <baptistedulong@gmail.com>
2024-05-07 16:18:39 +0200

    rajout des pages et tout est fonctionnel

---
+++ src/components/landing/landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import Header from "../header/header.jsx";
+
+function LandingPage() {
+    return (
+        <Header />
+    );
+}
+
+export default LandingPage;
