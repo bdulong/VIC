683bd0b856679c19ae2090072b4fb750919f82f7
bdulong <baptistedulong@gmail.com>
2024-05-08 15:20:47 +0200

    header en cours

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './header.css'
+import Logo from '../../images/logo.png';
 
 const Header = () => {
     return(
         <header>
             <nav>
-                <ul><Link to="/"></Link></ul>
+                <div className='header-content' >
+                    <ul><Link to="/"> <img className='logo' src={Logo} alt="Vers l'intelligence collective"></img></Link></ul>
+                    <div className='lien-redirection' >
+                        <ul>Qui sommes-nous ?</ul>
+                        <ul>Vos idées</ul>
+                        <ul>Nous rejoindre</ul>
+                    </div>
+                </div>
+                <span className='barre'></span>
             </nav>
         </header>
     )
