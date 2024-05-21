9c6a6511aaa30173bb5c3886f85b2073c486b3b3
bdulong <baptistedulong@gmail.com>
2024-05-22 01:18:54 +0200

    ajout avant hash link

--- src/components/assets/header/header.jsx
+++ src/components/assets/header/header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
                 <div className='header-content' >
                     <ul><Link to="/"> <img className='logo' src={Logo} alt="Vers l'intelligence collective"></img></Link></ul>
                     <div className='lien-redirection' >
-                    <Link to="/"><ul>Qui sommes-nous ?</ul></Link>
+                    <Link to="/#qui-sommes-nous"><ul>Qui sommes-nous ?</ul></Link>
                     <Link to="/vos-idees"><ul>Vos idées</ul></Link>
                     <Link to="/nous-rejoindre"><ul>Nous rejoindre</ul></Link>
                     </div>
