cb501e16f70c45146dfea9ee97c3dd0fe615e231
bdulong <baptistedulong@gmail.com>
2024-07-22 02:20:27 +0200

    img devient public

--- src/components/assets/header/header.jsx
+++ src/components/assets/header/header.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 import './header.css'
-import Logo from '../../../images/logo.png';
 
 const Header = () => {
     return(
         <header>
             <nav>
                 <div className='header-content' >
-                    <ul><Link to="/"> <img className='logo' src={Logo} alt="Vers l'intelligence collective"></img></Link></ul>
+                    <ul><Link to="/"> <img className='logo' src='./images/logo.png' alt="Vers l'intelligence collective"></img></Link></ul>
                     <div className='lien-redirection' >
                     <ul><HashLink smooth to='/#qui-sommes-nous'>Qui sommes-nous ?</HashLink></ul>                    
                     <Link to="/vos-idees"><ul>Vos idées</ul></Link>
