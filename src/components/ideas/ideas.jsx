ecc86f882ddf02470b37355beedafc447b028758
bdulong <baptistedulong@gmail.com>
2024-08-05 16:59:59 +0200

    up

--- src/components/ideas/ideas.jsx
+++ src/components/ideas/ideas.jsx
@@ -60,13 +60,69 @@ function IdeasPage() {
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className='appel-action-container'>
-                <div className='appel-action-text'>
-                    <p>Les partis politique ne font que diviser les parisiens. C'est seulement en unissant leurs forces que les Parisiens pourront  redonner tout son sens à la démocratie et façonner un avenir où chaque  voix compte vraiment.</p>
+                <div className='appel-action-container'>
+                    <div className='appel-action-text'>
+                        <p>Les partis politique ne font que diviser les parisiens. C'est seulement en unissant leurs forces que les Parisiens pourront  redonner tout son sens à la démocratie et façonner un avenir où chaque  voix compte vraiment.</p>
+                    </div>
+                    <div className='appel-action-cta'>
+                        <p>Rejoignez-nous</p>
+                    </div>
                 </div>
-                <div className='appel-action-cta'>
-                    <p>Rejoignez-nous</p>
+                <div className='idea-container'>
+                    <div className='idea-content'>
+                        <div className='user-info'>
+                            <img src='/images/user.png' alt="Utilisateur"/>
+                            <div className='idea-title'>
+                                <p>Jooidjfghoidjghn Doeiuhfgiuhdiugh</p>
+                                <h4>Lorem ipsum dolor sit amet dolor sit amet</h4>
+                            </div>
+                            <div className='idea-category'>
+                                <img src='/icons/tag.svg' alt="Catégorie"/>
+                                <h4 className='tag'>Transport</h4>
+                            </div>
+                        </div>
+                        <div className='idea-text-preview'>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat.</p>
+                        </div>
+                    </div>
+                    <div className='idea-interaction'>
+                        <div className='idea-vote'>
+                            <img src='/icons/upvote.svg' alt="Upvote"/>
+                            <p className='interaction'>9410</p>
+                        </div>
+                        <div className='idea-comment'>
+                            <img src='/icons/commentaire.svg' alt="Commentaire"/>
+                            <p className='interaction'>145</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='idea-container'>
+                    <div className='idea-content'>
+                        <div className='user-info'>
+                            <img src='/images/user.png' alt="Utilisateur"/>
+                            <div className='idea-title'>
+                                <p>Jooidjfghoidjghn Doeiuhfgiuhdiugh</p>
+                                <h4>Lorem ipsum dolor sit amet dolor sit amet</h4>
+                            </div>
+                            <div className='idea-category'>
+                                <img src='/icons/tag.svg' alt="Catégorie"/>
+                                <h4 className='tag'>Transport</h4>
+                            </div>
+                        </div>
+                        <div className='idea-text-preview'>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat.</p>
+                        </div>
+                    </div>
+                    <div className='idea-interaction'>
+                        <div className='idea-vote'>
+                            <img src='/icons/upvote.svg' alt="Upvote"/>
+                            <p className='interaction'>9410</p>
+                        </div>
+                        <div className='idea-comment'>
+                            <img src='/icons/commentaire.svg' alt="Commentaire"/>
+                            <p className='interaction'>145</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
