30a3c11a484396e74b3d6cd3cd4d2fc30490b57d
bdulong <baptistedulong@gmail.com>
2024-08-21 18:23:16 +0200

    ajout arrow pas sur pour category lorsque les catégories dépassent

--- src/components/ideas/ideas.jsx
+++ src/components/ideas/ideas.jsx
@@ -193,13 +193,19 @@ function IdeasPage() {
                     <p>Pour trouver une nouvelle voie, ou des gens d'opinions différentes</p>
                 </div>
                 <div className='category-container'>
+                    <div className='category-scroll-button category-scroll-button--left'>
+                        <img src='/icons/arrow.svg' alt="Défilement à gauche" />
+                    </div>
                     <div className='categories'>
                         {categories.map((category, index) => (
                             <ul key={index}>{category}</ul>
                         ))}
                     </div>
-                    <hr />
+                    <div className='category-scroll-button category-scroll-button--right'>
+                        <img src='/icons/arrow.svg' alt="Défilement à droite" />
+                    </div>
                 </div>
+                <hr />
             </div>
             <div className='ideas-list-container'>
                 {error && <p className="error-message">{error}</p>}
